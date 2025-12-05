f52d107dea72f70a9da19832a7c380949d9fdef6
k1105 <kntymgs1105@gmail.com>
2025-12-05 17:13:24 +0900

    ssr errorの修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import P5Canvas from "./components/P5Canvas";
-import ColorLayer from "./components/ColorLayer";
+import dynamic from 'next/dynamic';
+
+const P5Canvas = dynamic(() => import('./components/P5Canvas'), { ssr: false });
+const ColorLayer = dynamic(() => import('./components/ColorLayer'), { ssr: false });
 
 export default function Home() {
   return (
