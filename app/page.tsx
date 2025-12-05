61228e4edb3e1a58d24e3979ce6a0934caa242fe
k1105 <kntymgs1105@gmail.com>
2025-12-05 17:25:59 +0900

    qrコードを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 const P5Canvas = dynamic(() => import('./components/P5Canvas'), { ssr: false });
 const ColorLayer = dynamic(() => import('./components/ColorLayer'), { ssr: false });
@@ -10,6 +11,23 @@ export default function Home() {
     <>
       <ColorLayer />
       <P5Canvas />
+      <div
+        style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 1000,
+        }}
+      >
+        <Image
+          src="/qrcode.png"
+          alt="QR Code"
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
     </>
   );
 }
